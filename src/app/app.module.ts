d9c00716b21fc254235babd6ba3423918607f5b8
Alhmidi <abdulsattar.alhmidi@etu.unilim.fr>
2022-03-25 17:11:08 +0100

    Qr code Read

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
+import { Deeplinks } from '@awesome-cordova-plugins/deeplinks/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [InAppBrowser,Deeplinks,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,15 @@
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+
+import { AlertController } from '@ionic/angular';
+import {  NgZone } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { Deeplinks } from '@awesome-cordova-plugins/deeplinks/ngx';
+import { TreePage } from '../tree/tree.page';
+
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +18,64 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  scanActive?: boolean = false;
+  public textScanned?: string = '/tree/8';
+
+  constructor(private iab: InAppBrowser,
+    public alertController: AlertController,
+    private router: Router,
+    private zone: NgZone,
+    private route: ActivatedRoute,
+    private deeplinks: Deeplinks
+    ) {}
+
+
+  async checkPermission() {
+    return new Promise(async (resolve, reject) => {
+      const status = await BarcodeScanner.checkPermission({ force: true });
+      if (status.granted) {
+        resolve(true);
+      } else if (status.denied) {
+        BarcodeScanner.openAppSettings();
+        resolve(false);
+      }
+    });
+  }
+
+  async startScanner() {
+    const allowed = await this.checkPermission();
+
+    if (allowed) {
+      this.scanActive = true;
+     // BarcodeScanner.hideBackground();
+      BarcodeScanner.showBackground();
+
+      const result = await BarcodeScanner.startScan();
+
+      if (result.hasContent) {
+        this.scanActive = false;
+         //The QR content will come out here
+        //Handle the data as your heart desires here
+        this.textScanned = result.content;
+      } else {
+        alert('NO DATA FOUND!');
+      }
+    } else {
+      alert('NOT ALLOWED!');
+    }
+  }
+
+  stopScanner() {
+    BarcodeScanner.stopScan();
+    this.scanActive = false;
+  }
+
+  ionViewWillLeave() {
+    BarcodeScanner.stopScan();
+    this.scanActive = false;
+  }
+  openLink(link: any) {
+    this.route.url.pipe(link);
+  }
 
 }
