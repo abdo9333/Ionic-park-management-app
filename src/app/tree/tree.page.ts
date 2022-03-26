902b0c0bd1c5971a580151544645004e774ac1cb
Warda Fareha <79594409+wardafr@users.noreply.github.com>
2022-03-26 18:51:49 +0100

    Tree Map

--- src/app/tree/tree.page.ts
+++ src/app/tree/tree.page.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Tree } from './tree.model';
 import { TreeService } from '../services/tree.service';
+import { ViewChild, ElementRef } from '@angular/core';
+
+declare let google;
+
+interface Marker {
+  position: {
+    lat: number;
+    lng: number;
+  };
+  title: string;
+}
 
 @Component({
   selector: 'app-tree',
@@ -8,22 +19,56 @@ import { TreeService } from '../services/tree.service';
   styleUrls: ['./tree.page.scss'],
 })
 export class TreePage implements OnInit {
+  map: any;
 
   trees?: Tree[];
 
-  constructor(private treeService: TreeService) { }
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  @ViewChild('map', { read: ElementRef, static: false }) mapRef: ElementRef;
+
+  constructor(private treeService: TreeService) {}
 
   ngOnInit() {
     this.getAllTrees();
   }
-  getAllTrees(){
-    this.treeService.getAll()
-      .subscribe({
-        next: (data) => {
-          this.trees = data;
-        },
-        error: (e) => console.error(e)
-      });
+  getAllTrees() {
+    this.treeService.getAll().subscribe({
+      next: (data) => {
+        this.trees = data;
+      },
+      error: (e) => console.error(e),
+    });
+  }
+  ionViewDidEnter() {
+    this.showMap();
   }
 
+  showMap() {
+    const location = new google.maps.LatLng(
+      45.83760303980185,
+      1.238452736127495
+    );
+    const options = {
+      center: location,
+      zoom: 15,
+      disableDefaultUI: true,
+    };
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+    const marker = {
+      position: {
+        lat: 45.834731,
+        lng: 1.237997,
+      },
+      title: 'Sentier Tree',
+    };
+    this.addMarker(marker);
+  }
+
+  addMarker(marker: Marker) {
+    return new google.maps.Marker({
+      position: marker.position,
+      map: this.map,
+      title: marker.title,
+    });
+  }
 }
