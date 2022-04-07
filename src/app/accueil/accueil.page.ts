78da644dc48d76f7666d574a4f4075febd478b35
Alhmidi <abdulsattar.alhmidi@etu.unilim.fr>
2022-04-08 01:00:54 +0200

    latest update

--- src/app/accueil/accueil.page.ts
+++ src/app/accueil/accueil.page.ts
@@ -1,6 +1,17 @@
 import { Component } from '@angular/core';
 import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner/ngx';
 import { Router } from '@angular/router';
+import { ViewChild, ElementRef } from '@angular/core';
+
+declare let google;
+
+interface Marker {
+  position: {
+    lat: number;
+    lng: number;
+  };
+  title: string;
+}
 
 @Component({
   selector: 'app-accueil',
@@ -8,10 +19,46 @@ import { Router } from '@angular/router';
   styleUrls: ['./accueil.page.scss'],
 })
 export class AccueilPage {
+  map: any;
+  markers: Marker[] = [
+    {
+      position: {
+        lat: 45.834731,
+        lng: 1.237997,
+      },
+      title: 'Sentier Arbre',
+    },
+    {
+      position: {
+        lat: 45.83586,
+        lng: 1.237223,
+      },
+      title: 'Sentier bird',
+    },
+    {
+      position: {
+        lat: 45.835909,
+        lng: 1.236227,
+      },
+      title: 'Sentier Sport',
+    },
+    {
+      position: {
+        lat: 45.834933,
+        lng: 1.235905,
+      },
+      title: 'Sentier Handicap',
+    },
+  ];
 
   encodedData: any;
   scannedBarCode: any;
   barcodeScannerOptions: BarcodeScannerOptions;
+
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  @ViewChild('map', { read: ElementRef, static: false }) mapRef: ElementRef;
+
   constructor(private scanner: BarcodeScanner, private route: Router)
   {
     this.barcodeScannerOptions = {
@@ -34,6 +81,36 @@ export class AccueilPage {
 
   buttonClick(){
     this.route.navigate(['/bird']);
+  }
+  ionViewDidEnter() {
+    this.showMap();
+  }
+
+  showMap() {
+    const location = new google.maps.LatLng(
+      45.83760303980185,
+      1.238452736127495
+    );
+    const options = {
+      center: location,
+      zoom: 15,
+      disableDefaultUI: true,
+    };
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+    this.renderMarkers();
+  }
+
+  renderMarkers() {
+    this.markers.forEach((marker) => {
+      this.addMarker(marker);
+    });
+  }
+  addMarker(marker: Marker) {
+    return new google.maps.Marker({
+      position: marker.position,
+      map: this.map,
+      title: marker.title,
+    });
   }
 
 }
